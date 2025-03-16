5a9641abe6357bc804d86fe7c236688c225bbf81
Chinmay Patel <66440856+Chinmay-Patel10@users.noreply.github.com>
2025-03-16 01:32:11 -0400

    main.py

--- ESG_Analysis_Streamlit/main.py
+++ ESG_Analysis_Streamlit/main.py
@@ -8,8 +8,6 @@ st.title("ESD Data Analysis")
 st.header("Summary of Project")
 st.write("This a ESG Score Analysis of over 100 tech companies. Data Visualization find important insights about ESG score and its affect on Revenue, Tobin Q, and Market Cap.")
 
-Top_100_Tech_Final = pd.read_excel("Top_100_Tech_Final.xlsx")
-
 
 
 
